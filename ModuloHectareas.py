e3aa6c7e2a6c0c202f62632187331d253e497c49
Jhon Zambrano <jhonmacias1999@gmail.com>
2022-06-14 14:02:19 -0500

    Primer Commit, funcion mensaje usuario

---
+++ ModuloHectareas.py
@@ -0,0 +1,26 @@
+def mensajeUsuario():
+     """
+     Funcion que muestra un mensaje al usuario
+     mandamos como variable a estado objetivo
+     y el valor del costo
+     finalemente se muestr el mensaje al usuario
+     """
+
+     estadoObjetivo = {'A': '0', 'B':  '0', 'C': '0', 'D': '0', 'E': '0', 'F': '0','G': '0'} # estado objetivo
+  
+     costo = 0 # costo
+     # se muestra el mensaje al usuario
+     print(""" 
+     *****Estimado usuario, existen siete ubicaciones (Hectareas de plantación)*******
+         |-------------------------*****Hectarea_1 = 'A'*****-------------------------|
+         |-------------------------*****Hectarea_2 = 'B'*****-------------------------|
+         |-------------------------*****Hectarea_3 = 'C'*****-------------------------|
+         |-------------------------*****Hectarea_4 = 'D'*****-------------------------|
+         |-------------------------*****Hectarea_5 = 'E'*****-------------------------|
+         |-------------------------*****Hectarea_6 = 'F'*****-------------------------|
+         |-------------------------*****Hectarea_7 = 'G'*****-------------------------|
+         |-------------------------*****Hectarea_8 = 'H'*****-------------------------|
+     """)
+     
+     
+  
