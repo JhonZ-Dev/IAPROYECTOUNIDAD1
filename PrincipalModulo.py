af4c18ac11e8277f7c51318d86f87960961bb8dc
Jhon Zambrano <jhonmacias1999@gmail.com>
2022-06-14 15:09:28 -0500

    Creacion de modulos para llamar a la funciones

---
+++ PrincipalModulo.py
@@ -0,0 +1,8 @@
+import ModuloHectareas #Importar el modulo hectareas
+
+#Definir la función ingresoUsuarioDatos
+ModuloHectareas.mensajeUsuario() #Llamar a la función mensajeUsuario
+
+#---------------------------------------------------------------------------------#
+
+ModuloHectareas.ingresoUsuarioDatos() #Llamar a la función ingresoUsuarioDatos
